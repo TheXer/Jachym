4069361ac78fe632c599544c59570c9b2668f668
robertsokola <robertsokola2@gmail.com>
2022-03-13 18:28:51 +0100

    added manual testing generation code

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ bot = commands.AutoShardedBot(
     help_command=None)
 
 
+# This is important for adding new variable (database) to Bot instance so I can use it in cogs. 
 @bot.event
 async def on_ready():
     bot.pool = await create_pool(user=USER, password=PASSWORD, host=HOST, db=DATABASE)
