5997e8ea369454d906b0cbf22fae464a758b6ef0
robertsokola <robertsokola2@gmail.com>
2023-05-07 20:13:53 +0200

    reverted pools, this time both of them work
    Signed-off-by: robertsokola <robertsokola2@gmail.com>

--- cogs/poll_command.py
+++ cogs/poll_command.py
@@ -1,4 +1,7 @@
+import discord
+from discord import app_commands
 from discord.ext import commands
+from loguru import logger
 
 from src.db_folder.databases import PollDatabase, VoteButtonDatabase
 from src.jachym import Jachym
@@ -11,7 +14,7 @@ from src.ui.poll import Poll
 from src.ui.poll_view import PollView
 
 
-def error_handling(answer: tuple[str]) -> str:
+def error_handling(answer: list[str]) -> str:
     if len(answer) > Poll.MAX_OPTIONS:
         return f"Zadal jsi příliš mnoho odpovědí, můžeš maximálně {Poll.MAX_OPTIONS}!"
     elif len(answer) < Poll.MIN_OPTIONS:
@@ -19,26 +22,30 @@ def error_handling(answer: tuple[str]) -> str:
 
 
 class PollCreate(commands.Cog):
-    COOLDOWN = 10
-
     def __init__(self, bot: Jachym):
         self.bot = bot
 
-    @commands.command(aliases=["anketa"])
-    @commands.cooldown(1, COOLDOWN, commands.BucketType.user)
-    async def pool(self, ctx: commands.Context, question: str, *answer: str):
-        await ctx.message.delete()
+    @app_commands.command(
+        name="anketa",
+        description="Anketa pro hlasování. Jsou vidět všichni hlasovatelé.")
+    @app_commands.describe(
+        question="Otázka, na kterou potřebuješ vědět odpověď",
+        answer='Odpovědi, rozděluješ odpovědi uvozovkou ("), maximálně pouze 10 možností')
+    async def pool(self, interaction: discord.Interaction, question: str, answer: str) -> discord.Message:
 
-        message = await ctx.send(embed=PollEmbedBase("Dělám na tom, vydrž!"))
-        if error_handling(answer):
-            return await message.edit(embed=PollEmbedBase(error_handling(answer)))
+        await interaction.response.send_message(embed=PollEmbedBase("Dělám na tom, vydrž!"))
+        message = await interaction.original_response()
+
+        answers = answer.split(sep='"')
+        if error_handling(answers):
+            return await message.edit(embed=PollEmbedBase(error_handling(answers)))
 
         poll = Poll(
             message_id=message.id,
             channel_id=message.channel.id,
             question=question,
-            options=answer,
-            user_id=ctx.message.author.id
+            options=answers,
+            user_id=interaction.user.id
         )
 
         embed = PollEmbed(poll)
@@ -48,13 +55,38 @@ class PollCreate(commands.Cog):
 
         self.bot.active_discord_polls.add(poll)
         await self.bot.set_presence()
-
-        await message.edit(embed=embed, view=view)
+        logger.info(f"Successfully added Pool - {message.id}")
+        return await message.edit(embed=embed, view=view)
 
     @pool.error
     async def pool_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.send(embed=CooldownErrorEmbed(error.retry_after))
+
+    @commands.command()
+    async def anketa(self, ctx, question, *answers):
+        message = await ctx.send(embed=PollEmbedBase("Dělám na tom, vydrž!"))
+
+        if error_handling(answers):
+            return await message.edit(embed=PollEmbedBase(error_handling(answers)))
+
+        poll = Poll(
+            message_id=message.id,
+            channel_id=message.channel.id,
+            question=question,
+            options=answers,
+            user_id=ctx.user.id
+        )
+
+        embed = PollEmbed(poll)
+        view = PollView(poll, embed, db_poll=self.bot.pool)
+        await PollDatabase(self.bot.pool).add(poll)
+        await VoteButtonDatabase(self.bot.pool).add_options(poll)
+
+        self.bot.active_discord_polls.add(poll)
+        await self.bot.set_presence()
+        logger.info(f"Successfully added Pool - {message.id}")
+        return await message.edit(embed=embed, view=view)
 
 
 async def setup(bot):
