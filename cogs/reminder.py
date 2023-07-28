b85744dbeb82578c8bb4e44407ef5f13a223b6cc
Martin <81148675+Martian-0007@users.noreply.github.com>
2023-07-28 11:31:26 +0200

    Add dateparser and countdown function
    Added dateparser functionality and returns in how long the reminder will be triggered

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,5 +1,7 @@
 import asyncio
+from datetime import datetime, timedelta
 
+import dateparser
 import discord
 from discord.ext import Message, app_commands, commands
 
@@ -12,10 +14,44 @@ class Reminder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="reminder", description="Nastav si připomínku!")
-    @app_commands.describe(time="Za kolik minut se má připomínka zobrazit", topic="Předmět připomínky")
-    async def reminder(self, interaction: discord.Interaction, time: int, topic: str) -> Message:
+    def countdown(self, trigger_time: datetime) -> str:
+        """Calculate days, hours and minutes to a date.
+
+        Args:
+            trigger_time: The date.
+
+        Returns:
+            A string with the days, hours and minutes.
+        """
+        countdown_time: timedelta = trigger_time - datetime.now()
+
+        days, hours, minutes = (
+            countdown_time.days,
+            countdown_time.seconds // 3600,
+            countdown_time.seconds // 60 % 60,
+        )
+
+        # Return seconds if only seconds are left.
+        if days == 0 and hours == 0 and minutes == 0:
+            seconds: int = countdown_time.seconds % 60
+            return f"{seconds} second" + ("s" if seconds != 1 else "")
+
+        # TODO: Explain this.
+        return ", ".join(
+            f"{x} {y}{'s' * (x != 1)}"
+            for x, y in (
+                (days, "day"),
+                (hours, "hour"),
+                (minutes, "minute"),
+            )
+            if x
+        )
+
+    @app_commands.command(name="reminder", desription="Nastav si připomínku!")
+    @app_commands.describe(topic="Co ti mám připomenout?", time="Kdy ti to mám připomenout?")
+    async def reminder(self, interaction: discord.Interaction, time: str, topic: str) -> Message:
         """Reminder command"""
-        await interaction.response.send_message(f"Za {time} minut ti připomenu!", ephemeral=True)
-        await asyncio.sleep(time * 60)
+        trigger_time: datetime = dateparser.parse(time)
+        await interaction.response.send_message(f"Za {self.countdown(trigger_time)} ti připomenu {topic}!", ephemeral=True)
+        await asyncio.sleep(trigger_time.timestamp() - datetime.now().timestamp())
         await interaction.followup.send(f"{interaction.user.mention} připomínám ti {topic}!", ephemeral=True)
